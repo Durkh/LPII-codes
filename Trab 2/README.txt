c18c82b6a3b669b7a9f29c455d92fc80e07d34fd
Durkh <egidio15954@gmail.com>
2021-05-24 01:46:51 -0300

    trab 2 update

--- Trab 2/README.txt
+++ Trab 2/README.txt
@@ -1,18 +1,8 @@
-valores podem ser modificados na seção "DEFINES"
++-+-+-+-+-+-+-+-+-+-+-+- INFORMAÇÕES ÚTEIS +-+-+-+-+-+-+-+-+-+-+-+-
 
+	Valores podem ser modificados na seção "DEFINES", não recomendo mexer em outras seções.
 
-
-o layout do "parque" é mais ou menos esse
-
-fila geral		(indefinidas threads podem esperar aqui)
-fila de embarque 	(N_SEATS threads esperam para entrar no carro)
-carro			(N_SEATS threads andam no carro)
-
-no melhor caso possível assim que o carro é liberado, todas as threads da fila de embarque entram no carro e mais threads da fila geral passam pra fila de embarque, apesar que sendo sincero, do jeito que implementei a fila de embarque fecha antes chegar perto de lotar
-
-
-
-tentei dar uma melhorada nos prompts pra deixar o mais legível, a sintaxe é essa:
+	Tentei dar uma melhorada nos prompts pra deixar o mais legível, a sintaxe é essa:
 
 *** parque ***
 // carro //
@@ -20,8 +10,38 @@ tentei dar uma melhorada nos prompts pra deixar o mais legível, a sintaxe é es
 
 
 
++-+-+-+-+-+-+-+-+-+-+-+- NOTAS SOBRE O FUNCIONAMENTO DO PROGRAMA +-+-+-+-+-+-+-+-+-+-+-+-
 
-gcc version 10.2.0 (GCC)
+	O layout do "parque" é mais ou menos esse:
+
+fila geral		(indefinidas threads podem esperar aqui)
+fila de embarque 	(N_SEATS threads esperam para entrar no carro)
+carro			(N_SEATS threads andam no carro)
+
+No melhor caso possível assim que o carro é liberado, todas as threads da fila de embarque entram no carro e mais threads da fila geral passam pra fila de embarque, apesar que sendo sincero, do jeito que implementei a fila de embarque fecha antes chegar perto de lotar.
+
+
+Já as funções estão com esses papéis:
+
+main 		= entrada do parque, ela quem é responsável pela entrada dos visitantes
+ParkManager 	= controla a abertura e fechamento do parque
+Passenger	= comportamento de um passageiro da montanha russa
+Car		= carro da montanha russa
+
+	Criei o ParkManager por causa de um problema, como a main é a responsável por crias as threads dos passageiros e ela só passa a checar o fechamento do parque após a criação dos passageiros, se colocar um número muito grande de passageiros a main vai ficar presa muito tempo no loop criando as threads (e eu ainda adicionei um sleep opcional), nisso, se colocar números pequenos nos outros parâmetros, como "viagens para o parque fechar", "número de assentos" e "tempo da viagem", o programa completa as viagens para o parque fechar msa a main ainda está no loop criando as threads dos passageiros, logo não tem como checar a variável do limite de viagens e o programa continua rodando quando deveria ter parado. Nisso a criação do ParkManager garante que mesmo com apenas 1 viagem e muitos passageiros o parque ainda é fechado.
+
+
+	Além disso, quando o parque fecha, os visitantes que estão em fila não andam no brinquedo, só quem ficou no brinquedo.
+
+
+
++-+-+-+-+-+-+-+-+-+-+-+- NOTAS DE COMPILAÇÃO +-+-+-+-+-+-+-+-+-+-+-+-
 
 comando de compilação:
 gcc -pthread main.c -o main
+
+
+C11 Standard
+gcc version 10.2.0 (GCC)
+
+
--- Trab 2/main.c
+++ Trab 2/main.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <time.h>
 
+
 //  MACROS
 
 #define PRINT_AND_LINE(x, ...) printf(x "\n", ##__VA_ARGS__); \
@@ -12,59 +13,86 @@
 #define PUTS_AND_LINE(x)    puts(x); \
                             puts("...")
 
-//  DEFINES
+//  DEFINES YOU CAN TWEAK
 
 #define N_THREADS   25  //numero de passageiros
 #define N_SEATS     10  //numero de assentos no carro
 #define N_TRIPS     10  //numero de viagens até o parque fechar
 #define TRIP_TIME   10  //tempo em segundos da viagem do carro
 
+//velocidade da chegada dos visitantes
+//OBS: deixe só 1 descomentado
+#define FAST
+//#define MEDIUM
+//#define REALISTIC
+
+//  TYPE DEFINITIONS
+
+#ifdef FAST
+#define VISIT_RATE 1
+#endif
+
+#ifdef MEDIUM
+#define VISIT_RATE 2
+#endif
+
+#ifdef REALISTIC
+#define VISIT_RATE ((rand()%5)+5)
+#endif
+
 typedef unsigned uint;
 
 //  GLOBAL VARIABLES
 
 uint ticketNumber = 1, ticketCalled = 1, wallet[N_THREADS];
-uint trips;
-uint passengersAbroad;
 
 pthread_mutex_t carMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t boardingMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_barrier_t lineBarrier;
+pthread_barrier_t boardingBarrier;
+pthread_cond_t tripsCond = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t tripsMutex = PTHREAD_MUTEX_INITIALIZER;
 
 //   FUNCTIONS HEADERS
 
-_Noreturn void* Passenger(void* args);
-_Noreturn void* Car();
+void* Passenger(void* args);
+void* Car();
+void* ParkManager();
 
 
 int main() {
 
     srand(time(NULL));
 
+    //barreira para controlar se existem passageiros suficientes para o carro sair
     if(pthread_barrier_init(&lineBarrier, NULL, N_SEATS)){
         PUTS_AND_LINE("LINE LOCK INIT ERROR");
         exit(EXIT_FAILURE);
     }
 
+    //barreira inicializada com a quantidade de passageiros e o carro
+    if(pthread_barrier_init(&boardingBarrier, NULL, N_SEATS + 1)){
+        PUTS_AND_LINE("BOARDING LOCK INIT ERROR");
+        exit(EXIT_FAILURE);
+    }
 
-    pthread_t passengers[N_THREADS], car;
+
+    pthread_t passengers[N_THREADS], car, manager;
+
+    pthread_create(&manager, NULL, ParkManager, NULL);
 
     pthread_create(&car, NULL, Car, NULL);
 
     PUTS_AND_LINE(" *** Parque aberto, visitantes estão entrando ***");
-    for(long i=0; i<N_THREADS; i++){
+    for(long i=1; i<=N_THREADS; i++){
         pthread_create(&passengers[i], NULL, Passenger, (void*)i);
         //aos poucos os visitantes vão chegando no parque
-        sleep(1);
-        //sleep((rand()%5) + 5);
-
+        //coloquei esse sleep porque fica melhor de ver as coisas, mas se quiser colocar 0 no define, sem problemas
+        sleep(VISIT_RATE);
     }
 
-    while(trips < N_TRIPS);
+    pthread_join(manager, NULL);
 
-    PUTS_AND_LINE("*** Parque fechando, todos os visitantes devem deixar o local ***");
-
-    exit(EXIT_SUCCESS);
 }
 
 
@@ -94,7 +122,7 @@ _Noreturn void* Passenger(void* args){
         pthread_barrier_wait(&lineBarrier);
 
         PRINT_AND_LINE("-- Cliente %ld: estou embarcando no carro --", ID);
-        __sync_fetch_and_add(&passengersAbroad, 1);
+        pthread_barrier_wait(&boardingBarrier);
 
         //espera a volta acabar, o carro lockou o mutex, então o passageiro só prossegue após o carro desbloquear
         //"depois que entrou no carro só sai quando der a volta" esse é o ditado
@@ -102,7 +130,7 @@ _Noreturn void* Passenger(void* args){
         pthread_mutex_unlock(&carMutex);
 
         //sai do carro
-        __sync_fetch_and_sub(&passengersAbroad, 1);
+        pthread_barrier_wait(&boardingBarrier);
 
         PRINT_AND_LINE("Cliente %ld: saí do carro e vou passear no parque", ID);
         sleep((rand()%30) + 5); //numero entre 5 e 34
@@ -119,7 +147,7 @@ _Noreturn void* Car(){
         pthread_mutex_lock(&carMutex);
 
         //espera os passageiros entrarem
-        while(passengersAbroad < N_SEATS);
+        pthread_barrier_wait(&boardingBarrier);
 
         //carro cheio e pronto para sair, fecha o embarque
         pthread_mutex_lock(&boardingMutex);
@@ -131,10 +159,27 @@ _Noreturn void* Car(){
         pthread_mutex_unlock(&carMutex);
 
         //incrementa o contador de volta
-        trips++;
+        pthread_mutex_lock(&tripsMutex);
+        pthread_cond_signal(&tripsCond);
+        pthread_mutex_unlock(&tripsMutex);
+
         //espera os passageiros saírem
-        while(passengersAbroad != 0);
+        pthread_barrier_wait(&boardingBarrier);
         //libera o embarque
         pthread_mutex_unlock(&boardingMutex);
     }
+}
+
+_Noreturn void* ParkManager(){
+    pthread_mutex_lock(&tripsMutex);
+    //fica no for esperando sinal, quando recebe um sinal incrementa a variável e espera outro sinal
+    //quando recebeu sinais suficientes, retorna
+    for (uint trips = 0; trips < N_TRIPS; trips++){
+        pthread_cond_wait(&tripsCond, &tripsMutex);
+    }
+    pthread_mutex_unlock(&tripsMutex);
+
+    PUTS_AND_LINE("*** Parque fechando, todos os visitantes devem deixar o local ***");
+
+    exit(EXIT_SUCCESS);
 }
